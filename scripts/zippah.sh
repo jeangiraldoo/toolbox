b6522c9106b9c78f8430b83930b4cb96526c9ed0
Jean Giraldo <jeann.giraldo@gmail.com>
2025-11-29 12:51:37 -0500

    feat(scripts/zippah): ignore junk directories

--- scripts/zippah.sh
+++ scripts/zippah.sh
@@ -8,11 +8,13 @@
 # Author: Jean Giraldo
 # License: MIT
 
+shopt -s globstar
 script_name=$(basename "$0")
 dir_name="$(basename "$PWD")"
 
 TARGET_DIR="$HOME"
 FILE_NUMBER_THRESHOLD=1
+IGNORED_FILES=("venv" ".venv" "__pycache__" "*.egg-info*" "build")
 
 num_items_current_dir=$(find . -mindepth 1 -maxdepth 1 | wc -l)
 
@@ -26,12 +28,29 @@ echo -e "Total files: $num_items_current_dir\n"
 
 echo "[Action]"
 echo -e "$action\n"
-
 echo "[Logs]"
 
+included_items=()
+for raw_relative_file_path in ./**; do
+	simple_relative_file_path=${raw_relative_file_path#./}
+	should_ignore_file=false
+	for a in "${IGNORED_FILES[@]}"; do
+		if [[ "$simple_relative_file_path" == $a || "$simple_relative_file_path" == $a/* ]]; then
+			should_ignore_file=true
+			break
+		fi
+	done
+
+	if [[ "$should_ignore_file" == false ]]; then
+		included_items+=("$simple_relative_file_path")
+	fi
+done
+
 if [[ $action == "zip" ]]; then
-	zip -r "$TARGET_DIR/$dir_name.zip" .
+	zip "$TARGET_DIR/$dir_name.zip" "${included_items[@]}"
 	exit
 fi
 
-cp -r . "$TARGET_DIR"
+for item in "$included_items"; do
+	cp $item "$TARGET_DIR"
+done
